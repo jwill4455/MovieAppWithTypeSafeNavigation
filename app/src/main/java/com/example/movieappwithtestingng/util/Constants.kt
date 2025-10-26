558f469fb17cec0063458bfac7720b40ccafc9d4
jwill4455 <79523175+jwill4455@users.noreply.github.com>
2025-10-26 12:57:19 +0100

    Update Constants.kt

--- app/src/main/java/com/example/movieappwithtestingng/util/Constants.kt
+++ app/src/main/java/com/example/movieappwithtestingng/util/Constants.kt
@@ -2,6 +2,6 @@ package com.example.movieappwithtestingng.util
 
 object Constants {
    const val BASE_URL = "https://api.themoviedb.org/3/"
-   const val API_KEY = "b57133ad94ff91df87d9d4b3833d10d1"
+   const val API_KEY = "" // previously used test key (revoked), should be stored securely in .env or local.properties
    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 }
